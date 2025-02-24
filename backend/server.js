20b997358328b9f29b577832a10ccf218a0e6188
Isaac Oduh <ufuomaoduh@gmail.com>
2025-02-24 20:39:24 +0000

    feat: add middlewares - role, error, rate limit, authentication

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import connectionDB from "./config/connectDB.js";
 import { morganMiddleware, systemLogs } from "./utils/logger.js";
 import mongoSanitize from "express-mongo-sanitize";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 await connectionDB();
 
@@ -27,6 +28,9 @@ app.use(morganMiddleware);
 app.get("/api/v1/test", (req, res) => {
   res.json({ Hi: "Welcome to the Invoice App" });
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 1008;
 
