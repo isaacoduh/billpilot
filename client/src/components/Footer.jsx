3989fb1be7a5cdff0afd8b6793e60e709ec7171f
Isaac Oduh <ufuomaoduh@gmail.com>
2025-03-01 11:14:47 +0000

    feat: add auth and register api slice

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,7 +16,15 @@ function Copyright() {
 
 const Footer = () => {
   return (
-    <Box sx={{ position: "fixed", bottom: 0, width: "100%" }}>
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        bgcolor: "#000000",
+        marginTop: "auto",
+      }}
+    >
       <CssBaseline />
       <Box
         component="footer"
