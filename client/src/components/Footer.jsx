a68c86bc283d4e7fcdfc4e7393b35c47d545b742
Isaac Oduh <ufuomaoduh@gmail.com>
2025-03-01 10:46:43 +0000

    add common ui components and hooks

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import { Box, CssBaseline, Link, Typography } from "@mui/material";
+
+import { FaMoneyBillWave } from "react-icons/fa";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center" sx={{ color: "#ffffff" }}>
+      {"Copyright"}
+      <Link color="inherit" href="https://github.com/isaacoduh">
+        Bill Pilot
+      </Link>
+      {new Date().getFullYear()} {"."}
+    </Typography>
+  );
+}
+
+const Footer = () => {
+  return (
+    <Box sx={{ position: "fixed", bottom: 0, width: "100%" }}>
+      <CssBaseline />
+      <Box
+        component="footer"
+        sx={{ py: 1, px: 1, mt: "auto", bgColor: "#000000" }}
+      >
+        <Typography
+          variant="subtitle1"
+          align="center"
+          component="p"
+          sx={{ color: "#07f011" }}
+        >
+          <FaMoneyBillWave />
+          <FaMoneyBillWave />
+        </Typography>
+        <Copyright />
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
